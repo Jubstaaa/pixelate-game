30e01a6fc131de8080a65811cc154ed3a10b0a1f
jubstaaa <ilkerbalcilartr@gmail.com>
2024-11-09 17:05:52 +0300

    new categories

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -21,7 +21,7 @@ function CategoryCard({ item }) {
       <Image
         removeWrapper
         alt="Woman listing to music"
-        className="w-full h-full object-contain p-4"
+        className="w-full h-full object-contain p-4 aspect-square"
         src={item.icon}
       />
       <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 flex-col lg:flex-row gap-1">
